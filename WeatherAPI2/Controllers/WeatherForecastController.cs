79623fb2a5780c4885ab623de8c5f3277989d162
leonov_an <leonard@lenta.ru>
2021-08-04 23:19:47 +0400

    files for lesson 1- test

--- WeatherAPI2/Controllers/WeatherForecastController.cs
+++ WeatherAPI2/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WeatherAPI2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "TestGIT"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
